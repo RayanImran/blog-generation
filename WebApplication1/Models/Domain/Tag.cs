7e3e675b0ed0ac8326f2f53e18bfc4f034008750
Rayan Imran <rayanimran307@gmail.com>
2024-10-06 23:23:52 -0500

    dot net added models

---
+++ WebApplication1/Models/Domain/Tag.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Models.Domain
+{
+    public class Tag
+    {
+        public Guid Id{ get; set; }
+        public string Name{ get; set; }
+        public string DisplayName{ get; set; }
+    }
+}
